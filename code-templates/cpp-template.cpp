828f05b17c62d3123ce717fa2701329bfeb268e7
yongrae <neurosilver@gmail.com>
2017-01-28 22:03:14 +0900

    add cpp template

---
+++ code-templates/cpp-template.cpp
@@ -0,0 +1,58 @@
+// input, output 
+#include <iostream>
+#include <cmath>
+// container
+#include <vector>
+#include <array>
+#include <list>
+#include <map>
+#include <set>
+// container adaper
+#include <stack>
+#include <queue>
+// STL algorithm related
+#include <algorithm>
+#include <iterator>
+#include <functional>
+#include <numeric>
+#include <memory>
+#include <utility>
+
+//
+// #define DEBUG
+using namespace std;
+// Declaration of variables
+
+
+// Declaration of auxiliary function
+class genProbSolver {
+
+    public:
+        genProbSolver(string s) : name(s) {}
+        void process_input(istream &);
+        void process_solution();
+        void process_output(ostream &);
+        void echo_input();
+    private:
+        string name;
+};
+void genProbSolver::process_input(istream &pin) {
+
+}
+void genProbSolver::echo_input() {
+
+}
+void genProbSolver::process_solution() {
+
+}
+void genProbSolver::process_output(ostream &pout) {
+
+}
+int main(int argc, const char *argv[]) {
+    genProbSolver probsolver("");
+    probsolver.process_input(std::cin);
+    probsolver.echo_input();
+    probsolver.process_solution();
+    probsolver.process_output(std::cout);
+    return 0;
+}
