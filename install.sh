504049ec403c1b9dbcf405ded8e95bf09e40ae11
yongrae <neurosilver@gmail.com>
2017-04-24 00:32:48 +0900

    think boilr setting is better

--- install.sh
+++ install.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
 path=$HOME/bin
+
 echo "copying macm script to $HOME/bin"
 cp macm $HOME/bin/
+
+if [ ! -d $HOME/.config/macm ]; then
+    mkdir -p $HOME/.config/macm/code-templates
+fi
+
+cp ./code-templates/ $HOME/.config/macm/ -r
