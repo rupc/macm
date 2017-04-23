43e38cb4c4da62884f0680e675b5af8a5c2bff7a
yongrae <neurosilver@gmail.com>
2017-04-23 15:16:07 +0900

    simplized install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-if [ -d ~/hbin ]; then
-    #statements
-    if [ -f ~/hbin/macm ]; then
-        #statements
-        echo "macm is already installed."
-    fi
-    echo "copying macm script to $HOME/hbin"
-    cp macm ~/hbin
-else
-    echo "copying macm script to $HOME/.local/bin"
-    cp macm $HOME/.local/bin
-fi
+path=$HOME/bin
+echo "copying macm script to $HOME/bin"
+cp macm $HOME/bin/
